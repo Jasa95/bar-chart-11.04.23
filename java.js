67d59eb9b2e67d98873d1ad231395b82065645a1
Jasa95 <jaco3020@stud.kea.dk>
2023-04-25 14:24:48 +0200

    Update java.js

--- java.js
+++ java.js
@@ -3,16 +3,31 @@
 
 window.addEventListener("load", start);
 
-let 
+let count = 0;
+const charArray = [];
 
 function start() {
-    
+    const costumers = getQueuedData();
+
+    setInterval[() => addQueuedData(costumers), 1000];
+   
 }
 
+function addQueuedData(costumers) {
+    const queueSize = getNumberOfCostumers();
+    costumers.push(queueSize);
+    costumers.shift
+}
+
+
+function startChar() {
+    for (let i = 0; i < 40; i++); {
+        let number = getNumberOfCostumers();
+        charArray.push(number);
+    }
+}
 
 function getNumberOfCostumers() {
     //fake number total random
     return Math.floor(Math.random()*32);
 }
-
-const queueSize = getNumberOfCostumers();
